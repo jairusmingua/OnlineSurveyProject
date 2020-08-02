a77b8dded00f81da135b6045fbef43856b3a436a
Jairus <minguajairus@yahoo.com.ph>
2020-08-02 16:25:52 +0800

    dashboard ui fix temporary removed navbar

--- OnlineSurveyProject/Dashboard.Master.designer.cs
+++ OnlineSurveyProject/Dashboard.Master.designer.cs
@@ -13,15 +13,6 @@ namespace OnlineSurveyProject {
     public partial class Dashboard1 {
         
         /// <summary>
-        /// username control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor username;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
@@ -29,5 +20,14 @@ namespace OnlineSurveyProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// username control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor username;
     }
 }
